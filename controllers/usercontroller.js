8b80b92811d4214445e02f384debc05643dc3be2
MyGit <vivekkushwaha734@gmail.com>
2025-02-08 00:17:29 +0530

    Fixing logout cookie

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -251,7 +251,8 @@ exports.userLogin = async (req, res) => {
             expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
             httpOnly: true,
             signed: true,
-            secure:true,
+            secure: true,
+            domain: ".autointerv-backend.vercel.app",
             //   // Required for HTTPS (Vercel)
             sameSite: "None",
            
